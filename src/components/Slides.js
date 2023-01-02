adbd0c7f5fd2ec091cb8f56fcdbc931d747f9fda
Rishi Verma <rishianand001@gmail.com>
2023-01-02 18:22:16 +0530

    completed

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Slides({slides}) {
+
+    let [slideState, setSlideSlide] = useState(0);
+
+    let restart =()=>{
+        setSlideSlide(0)
+    }
+
+    let prev =()=>{
+        if(slideState > 0){
+            setSlideSlide((prevState)=> prevState - 1);
+        }
+    }
+    let next =()=>{
+        if(slideState < slides.length-1){
+            setSlideSlide((prevState)=> prevState + 1);
+        }
+    }
+
 
     return (
         <div>
             <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
+                <button data-testid="button-restart" onClick={restart} className={`${slideState !== 0 ? "small":"small outlined"}`} >Restart</button>
+                <button data-testid="button-prev" onClick={prev} className={`${slideState !== 0 ? "small":"small outlined"}`}>Prev</button>
+                <button data-testid="button-next" onClick={next} className={`${slideState !== slides.length -1 ? "small":"small outlined"}`}>Next</button>
             </div>
-            <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
-            </div>
+            {slides.map((slide, index)=>{
+                return ( slideState === index ? <div id="slide" key={index} className="card text-center">
+                            <h1 data-testid="title">{slide.title}</h1>
+                            <p data-testid="text">{slide.text}</p>
+                        </div>:'')
+            })
+
+            }
+            
         </div>
     );
 
